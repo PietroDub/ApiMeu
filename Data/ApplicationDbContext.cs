45729584ccccca95f3b4981832354fee02d8c794
PietroDub <pietroti453@gmail.com>
2024-11-01 12:00:14 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiMeu.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        DbSet<Cliente> Cliente { get; set; }
+    }
+}
